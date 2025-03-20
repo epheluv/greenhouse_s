ac23de6e3675f8e41ba586f07ada7fb4383c930a
epheluv <xzh674261804@163.com>
2025-03-20 15:52:23 +0800

    sth. back

---
+++ src/main/java/epheluv/agri/demo/CorsConfig.java
@@ -0,0 +1,22 @@
+package epheluv.agri.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// 新增配置类
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:8081") 
+                        .allowedMethods("*");
+            }
+        };
+    }
+}
